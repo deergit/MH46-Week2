86d17e1f4d44e323457e52f8279e20e8cd377723
deergod <CommonTroll@live.co.uk>
2023-03-10 11:24:14 +0000

    Uploading Friday's js file - Objects

---
+++ objects.js
@@ -0,0 +1,14 @@
+const cafe = {
+    name: "Whitesheep",
+    seatingCapacity: 100,
+    hasSpecialOffers: true,
+    drinks: [
+        "Cappuccino",
+        "Latte",
+        "Filter Coffee",
+        "Tea",
+        "Hot Chocolate"
+    ]
+};
+
+console.log(cafe);
