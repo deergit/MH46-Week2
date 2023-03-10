b2430fc4a7fab88eba57793fe61f77af7ac090ae
deergod <CommonTroll@live.co.uk>
2023-03-10 11:52:09 +0000

    further updating first activity in objects.js

--- objects.js
+++ objects.js
@@ -1,3 +1,25 @@
+/************************************************
+Object examples:
+************************************************/
+
+// mini activity
+const me = {
+    name: "Josh",
+    age: 27,
+    tired: true
+};
+
+console.log(`
+My name is ${me.name}
+I am ${me.age} years old (christ)
+${me.tired ? "I am tired" : "I'm feeling funky fresh"}
+`);
+
+// Example 1:
+
+let offer = "none";
+let time = 1200;
+
 const cafe = {
     name: "Whitesheep",
     seatingCapacity: 100,
@@ -8,20 +30,17 @@ const cafe = {
         "Filter Coffee",
         "Tea",
         "Hot Chocolate"
-    ]
+    ],
+
+    breakfastOffer: "Free croissant with coffee",
+    lunchOffer: "Free drink with surprisingly priced sandwich",
+    noOffer: "Sorry, no offer"
 };
 
-console.log(cafe);
-
-const me = {
-    name: "Josh",
-    age: 27,
-    tired: true
-};
-
-console.log(me);
-console.log(`
-My name is ${me.name}
-I am ${me.age} years old (christ)
-${me.tired ? "I am tired" : "I'm feeling funky fresh"}
-`);
+if (time < 1100) {
+    offer = cafe.breakfastOffer;
+    console.log(`\n${offer}\n`);
+} else if (time < 1500) {
+    offer = cafe.lunchOffer;
+    console.log(`\n${offer}\n`);
+}
